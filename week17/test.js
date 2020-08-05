66f888bdbfa93ed15c6aa99fcb04d7e2539f3741
qintian <835395495@qq.com>
2020-08-06 00:38:01 +0800

    add debugger

--- week17/test.js
+++ week17/test.js
@@ -1,16 +1,19 @@
 let y = g => 
     (f => f(f))(
       self =>
-        g( (...args) => {
-            console.log(args);
-            self(self).apply(this, args)
-        } )
+        // 当y(g) 被调用之后
+        // self(self) 等同于f(f) 也就是函数g(f) 
+        g( (...args) => self(self).apply(this, args))
     )
 
     
-let f = y(self => {
-    console.log(self)
-  return n => n > 0 ? self(n - 1) + n : 0
-})
+// f通过y包裹函数调用之后返回的是参数cb()返回的函数
 
+let f = y(self => {
+  return n => {
+     // self 调用的是当前匿名函数 这里self 等于f
+      const val = n > 0 ? self(n - 1)  + n : 0;
+      return val;
+  }
+})
 console.log(f(100))
